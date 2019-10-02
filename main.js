c68bd557404d94262409e1ed7a4682bfc5ada8e8
Mr-Jess <jesse.passeri@codeimmersives.com>
2019-10-02 09:37:01 -0400

    completed instructor height

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 function isItLong(str) {
-  if (str.length > 20){
+  if (str.length > 20) {
     return "That's a long string!"
   }
 }
 
 function isItMedium(str) {
-  if (str.length >=10 && str.length <=20){
+  if (str.length >=10 && str.length <=20) {
     return "That's a regular sized string!"
   }
 }
 
 function isItShort(str) {
-  if (str.length <= 10){
+  if (str.length <= 10) {
     return "That's a small string!"
   }
 }
 
 function howLongIsMyString(str) {
-  if (str.length <=10 && str.length ){
+  if (str.length >=10 && str.length <=20) {
+    return "That's a regular sized string!";
+  }
+  else if (str.length > 20) {
     return "That's a long string!"
-    return "That's a regular sized string!"
-    return  
+  }
+  else if (str.length < 10) {
+    return "That's a small string!"
   }
 }
 
