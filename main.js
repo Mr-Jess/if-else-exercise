511af6fed51ca2950d80c747415ce7dee3ccd805
Mr-Jess <jesse.passeri@codeimmersives.com>
2019-10-01 22:20:53 -0400

    if else excer

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
-function isItLong() {
-
+function isItLong(str) {
+  if (str.length > 20){
+    return "That's a long string!"
+  }
 }
 
-function isItMedium() {
-
+function isItMedium(str) {
+  if (str.length >=10 && str.length <=20){
+    return "That's a regular sized string!"
+  }
 }
 
-function isItShort() {
-
+function isItShort(str) {
+  if (str.length <= 10){
+    return "That's a small string!"
+  }
 }
 
-function howLongIsMyString() {
-
+function howLongIsMyString(str) {
+  if (str.length <=10 && str.length ){
+    return "That's a long string!"
+    return "That's a regular sized string!"
+    return  
+  }
 }
 
-function instructorHeight() {
-
+function instructorHeight(str) {
+    if(str.length > 20) {
+      return "That's a long string";
+    } else if(str.length >=10 && str.length <=20) {
+      return "That's a regular sized string!";
+    } else if(str.length <= 10) {
+      return "That's a short string!";
+    } else {
+      return str;
+    }
+  }
+  
+  console.log(instructorHeight(64));
 }
 
 
